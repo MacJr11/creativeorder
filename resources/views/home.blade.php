f06708a084f3f7a0ea1405849a6c99b975203b6a
MacJr Kyeremeh <kyeremehjr11@gmail.com>
2025-07-05 08:18:45 -1200

    Initial Laravel project commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yo let's start</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <h1>Welcome Kelly</h1>
+    <p>This is Just a home page lets start work.</p>
+</body>
+</html>
